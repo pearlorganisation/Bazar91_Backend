0dd136190679913e7dee57dfcf1ddd788e12e95b
Shashankpearl <shashanknegi@pearlorganisation.com>
2024-11-15 18:54:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,23 +25,23 @@ app.use(
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
-            "https://bazar91-frontend.vercel.app"
+            "https://bazar91-frontend.vercel.app/"
           ],
           credentials: true,
           methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "HEAD"],
           allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
-          exposedHeaders: ["*", "Authorization"],
+          exposedHeaders: ["Authorization"],
         }
       : {
           origin: [
             "http://localhost:5173",
             "http://localhost:5174",
-            "http://localhost:5175","https://bazar91-frontend.vercel.app"
+            "http://localhost:5175","https://bazar91-frontend.vercel.app/"
             "*"],
         credentials:true,
         methods:["GET","PUT","POST","PATCH","DELETE","HEAD"],
         allowedHeaders:["Content-Type","Authorization","x-csrf-token"],
-        exposedHeaders:["*","Authorization"]
+        exposedHeaders:["Authorization"]
     }
 );
 
