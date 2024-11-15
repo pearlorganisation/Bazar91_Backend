235235ea00559b6cc406779e11623dd94ed83b90
Saurabh Chamola <saurabh@pearlorganisation.com>
2024-11-15 16:31:43 +0530

    minor fix

--- index.js
+++ index.js
@@ -12,56 +12,60 @@ import { ProductRouter } from "./src/Routes/product.js";
 
 configDotenv();
 const app = express();
-const PORT = process.env.PORT||9002 ;
-
+const PORT = process.env.PORT || 9002;
 
 // ----------------------------CORS HANDLING -------------------------------------------------------------
-app.use(cors(
-    process.env.NODE_ENV === "development" ? {
-        origin : [ 
-            "http://localhost:7001",
-            "http://localhost:7002",
-            "*"],
-        credentials:true,
-        methods:["GET","PUT","POST","PATCH","DELETE","HEADER"],
-        allowedHeaders:["Content-Type","Authorization","x-csrf-token"],
-        exposedHeaders:["*","Authorization"]
-    }:{
-        origin:[
+app.use(
+  cors(
+    process.env.NODE_ENV === "development"
+      ? {
+          origin: [
             "http://localhost:7001",
             "http://localhost:7002",
             "http://localhost:5173",
             "http://localhost:5174",
             "http://localhost:5175",
-        ],
-        methods:["GET","POST","PUT","PATCH","DELETE"],
-        allowedHeaders:["Authorization","Content-Type","x-csrf-token"],
-        exposedHeaders:["*","Authorization"]
-
-    }
-));
+          ],
+          credentials: true,
+          methods: ["GET", "PUT", "POST", "PATCH", "DELETE", "HEADER"],
+          allowedHeaders: ["Content-Type", "Authorization", "x-csrf-token"],
+          exposedHeaders: ["*", "Authorization"],
+        }
+      : {
+          origin: [
+            "http://localhost:7001",
+            "http://localhost:7002",
+            "http://localhost:5173",
+            "http://localhost:5174",
+            "http://localhost:5175",
+          ],
+          methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+          allowedHeaders: ["Authorization", "Content-Type", "x-csrf-token"],
+          exposedHeaders: ["*", "Authorization"],
+        }
+  )
+);
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get("/api/v1",async (req,res)=>{
-    res.status(200).json({status:true,message:"Welcome to Bazzar91"});
-})
+app.get("/api/v1", async (req, res) => {
+  res.status(200).json({ status: true, message: "Welcome to Bazzar91" });
+});
 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/brand", brandRouter);
+app.use("/api/v1/product", ProductRouter);
 
-app.use("/api/v1/auth",authRoutes)
-app.use("/api/v1/brand",brandRouter);
-app.use("/api/v1/product",ProductRouter);
-
-app.get("*",async (req,res)=>{
-  res.status(200).json({message:"Path Not Defined !!"});
-})
+app.get("*", async (req, res) => {
+  res.status(200).json({ message: "Path Not Defined !!" });
+});
 
 app.use(errorHandler);
 
-app.listen(PORT,()=>{
-    mongoDbConnnection();
+app.listen(PORT, () => {
+  mongoDbConnnection();
 
-    console.log(chalk.bold.bgGreenBright(`Server Is Running at ${PORT}`));
-})
+  console.log(chalk.bold.bgGreenBright(`Server Is Running at ${PORT}`));
+});
--- src/Models/brand.js
+++ src/Models/brand.js
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
-const brandSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        minlength:[5,"Min Length For Brand Title is 5"],
-        required:[true,"Title Is Required Field !!"]
+const brandSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      minlength: [5, "Min Length For Brand Title is 5"],
+      required: [true, "Title Is Required Field !!"],
     },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-},{
-    timestamps:true
-});
-
-export const BrandModel = new mongoose.model("Brand",brandSchema);
+export const BrandModel = new mongoose.model("Brand", brandSchema);
